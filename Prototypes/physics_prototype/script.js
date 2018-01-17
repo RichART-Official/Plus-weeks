55e8e367cd9631ffb6f653f59a3fb6423d6e6a44
Richard van 't Hof <richardvanthof@live.nl>
2018-01-17 20:34:26 +0100

    Tried to add make a box launch, didn't work :(

--- Prototypes/physics_prototype/script.js
+++ Prototypes/physics_prototype/script.js
@@ -8,24 +8,49 @@ var Engine = Matter.Engine,
 var engine;
 var world;
 var box1;
+var ground;
 
 function setup() {
-  
-  createCanvas(400,400);
+
+  createCanvas(500,500);
   background(20);
   engine = Engine.create();
   world = engine.world;
-  box1 = Bodies.rectangle(200, 200, 100, 100);
-  
+  ground = Bodies.rectangle(250, height, width, 10, { isStatic: true});
+  box1 = new Box(200, 100, 50, 50);
+
   // run the engine
   Engine.run(engine);
   // add all of the bodies to the world
-   World.add(engine.world, box1);
-  
-  console.log(box1);
+   World.add(engine.world, ground);
+
+  //console.log(box1);
 }
 
 function draw() {
-  console.log(box1.position.x);
-  rect(box1.position.x, box1.position.y, 80,80);
+  //console.log(box1.position.x);
+  //rect(box1.position.x, box1.position.y, 80,80);
+  Matter.Body.applyForce(box1, { x: 0, y: 0 }, { x: 0.1, y: 0 });
+  box1.show();
+}
+
+function Box(x, y, w, h){
+  this.body = Bodies.rectangle(x, y, w, h);
+
+  this.w = w;
+  this.h = h
+  this.x = x;
+  this.y = y;
+  World.add(world, this.body);
+
+  this.show = function(){
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rect(0, 0, this.w, this.h);
+
+    pop();
+  }
 }
